a9837d5a2c7f8c02266ef987d25a8fa0d56ed9ed
Michael Decker <michael_decker@gmx.net>
2020-12-22 09:42:49 +0100

    added simple test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module bitbucket.org/md_ed/go_test
+
+go 1.14
---
+++ main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	ans := Hello()
+	if ans != "Hello World" {
+		t.Errorf("Hello() = %s; want Hello World", ans)
+	}
+}
